6a4af359875246c88540f3525bd78e27b4647967
CnnMts <matis.conan@coda-student.school>
2024-11-21 12:09:57 +0100

    0.0.4(update entities)

--- src/entities.lua
+++ src/entities.lua
@@ -1,4 +1,21 @@
-Entity = {}
 ENTITIES = {}
+Entity.__index= ENTITIES
 
-Entity.__index = Entity
+function ENTITIES:new(type,id,x,y,w,h)
+    local instance = setmetatable({},ENTITIES)
+    instance.type = type
+    instance.id = id
+    instance.x = x
+    instance.y = y
+    instance.w = 32
+    instance.h = 32
+return instance
+end
+
+function ENTITIES.update(dt)
+
+end
+
+function ENTITIES.draw()
+
+end
